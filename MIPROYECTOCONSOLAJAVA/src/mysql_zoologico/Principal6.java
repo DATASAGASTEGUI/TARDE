0e812479613aad6ba716639768e807dd8af4d300
Walter Ismael Sagástegui Lescano <datasagamadrid@gmail.com>
2022-11-04 18:22:36 +0100

    TITULO

--- MIPROYECTOCONSOLAJAVA/src/mysql_zoologico/Principal6.java
+++ MIPROYECTOCONSOLAJAVA/src/mysql_zoologico/Principal6.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Principal6 {
 
@@ -14,25 +17,46 @@ public class Principal6 {
         String usuario = "root";
         String clave = "12345678";
 
+        List<String> tablas_al = new ArrayList<String>();
+        String query1 = "SHOW TABLES";
+        String query2 = "SHOW COLUMNS FROM";
+        PreparedStatement ps = null;
+
         try {
             Connection conexion1 = DriverManager.getConnection(url1, usuario, clave);
             Connection conexion2 = DriverManager.getConnection(url2, usuario, clave);
 
             if (conexion1 != null && conexion2 != null) {
                 //1. MOSTRAR TODAS LAS TABLAS DE LA BASE DE DATOS ZOOLOGICO
-                String query = "SHOW TABLES";
+
                 try {
-                     PreparedStatement ps = conexion1.prepareStatement(query);
-                     ResultSet rs = ps.executeQuery();
-                     while(rs.next()) {
-                         //String tabla = rs.getString(1);
-                         String tabla = rs.getString("Tables_in_zoologico");
-                         System.out.println(tabla);
-                     }
-                }catch(Exception e) {
-                
+                    ps = conexion1.prepareStatement(query1);
+                    ResultSet rs = ps.executeQuery();
+                    while (rs.next()) {
+                        String tabla = rs.getString("Tables_in_zoologico");//String tabla = rs.getString(1);
+                        tablas_al.add(tabla);
+                    }
+                    //System.out.println(Arrays.asList(tablas_al));
+                    for(String s: tablas_al) {
+                        String s1 = query2 + " " + s;
+                        ps = conexion1.prepareStatement(s1);
+                        ResultSet rs2 = ps.executeQuery();
+                        while(rs2.next()) {
+                           String columna = rs2.getString(1);
+                           System.out.println(columna);
+                        }
+                        s1 = "";
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                } catch (Exception e) {
+
                 }
-                
+
                 System.out.println("OK: CONEXION " + conexion1.getCatalog() + "  " + conexion2.getCatalog());
 
                 conexion1.close();
